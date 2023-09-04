d0a0f3dc420bcfa89682ef30bb4a0f56ce0f4810
hanielchids <hanielchids@gmail.com>
2023-09-04 16:52:03 +0200

    feat(screens): updates t screen components

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,15 +5,13 @@ import {
   FlatList,
   Text,
   View,
-  StyleSheet,
   StatusBar,
-  TouchableHighlight,
   ActivityIndicator,
 } from "react-native";
-import LottieView from "lottie-react-native";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { weatherConditions } from "../utils/weatherConditions";
+import NetInfo from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Location from "expo-location";
+import styles from "../styles/styles";
 
 const api = {
   key: "",
@@ -22,11 +20,6 @@ const api = {
 
 const HomeScreen = ({ navigation }) => {
   const [errorMsg, setErrorMsg] = useState(null);
-  const [city, setCity] = useState(null);
-  const [coordinates, setCoordinates] = useState({
-    long: null,
-    lat: null,
-  });
   const [temperature, setTemperature] = useState({
     min: null,
     max: null,
@@ -51,6 +44,22 @@ const HomeScreen = ({ navigation }) => {
       try {
         setLoading(true);
 
+        const isConnected = await NetInfo.fetch().isConnected;
+
+        if (!isConnected) {
+          // If not connected to the internet, load data from local storage
+          const storedWeatherData = await AsyncStorage.getItem("weatherData");
+
+          if (storedWeatherData) {
+            const parsedData = JSON.parse(storedWeatherData);
+            setWeatherDetails(parsedData.weatherDetails);
+            setTemperature(parsedData.temperature);
+            setForecast(parsedData.forecast);
+            setLoading(false);
+            return;
+          }
+        }
+
         const { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== "granted") {
           setErrorMsg("Permission to access location was denied");
@@ -59,15 +68,6 @@ const HomeScreen = ({ navigation }) => {
 
         const location = await Location.getCurrentPositionAsync({});
         const { latitude, longitude } = location.coords;
-
-        const regionName = await Location.reverseGeocodeAsync({
-          longitude,
-          latitude,
-        });
-        const cityName = regionName[0].city;
-
-        setCoordinates({ lat: latitude, long: longitude });
-        setCity(cityName);
 
         const weatherResponse = await fetch(
           `${api.base}weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=adee2681c1b31134e040b6e42bd659d1`
@@ -78,7 +78,7 @@ const HomeScreen = ({ navigation }) => {
           setWeatherDetails({
             temp: Math.round(weatherData.main.temp),
             wea: weatherData.weather[0].main,
-            city: cityName,
+            city: weather.name,
             country: weatherData.sys.country,
           });
 
@@ -86,6 +86,17 @@ const HomeScreen = ({ navigation }) => {
             min: Math.round(weatherData.main.temp_min),
             max: Math.round(weatherData.main.temp_max),
           });
+
+          const weatherDataToStore = {
+            weatherDetails,
+            temperature,
+            forecast,
+          };
+
+          await AsyncStorage.setItem(
+            "weatherData",
+            JSON.stringify(weatherDataToStore)
+          );
 
           setLoading(false);
 
@@ -113,49 +124,63 @@ const HomeScreen = ({ navigation }) => {
         }
       } catch (error) {
         console.error("Error fetching data:", error);
+        console.error("Error fetching data:", errorMsg);
       }
     };
 
     fetchData();
   }, []);
 
-  let source, bgsource;
+  let source, bgsource, bgcolor;
 
   switch (weatherDetails.wea) {
     case "Rain":
       bgsource = require("../assets/backgrounds/sea_rainy.png");
       source = require("../assets/icons/rain2.png");
+      bgcolor = "#57575D";
       break;
     case "Clear":
       bgsource = require("../assets/backgrounds/sea_sunnypng.png");
       source = require("../assets/icons/clear2.png");
+      bgcolor = "#4a90e2";
+      break;
+    case "Sunny":
+      bgsource = require("../assets/backgrounds/sea_cloudy.png");
+      source = require("../assets/icons/partlysunny2.png");
+      bgcolor = "#47AB2F";
       break;
     case "Clouds":
       bgsource = require("../assets/backgrounds/sea_cloudy.png");
       source = require("../assets/icons/partlysunny2.png");
+      bgcolor = "#54717A";
       break;
     case "Thunderstorm":
       bgsource = require("../assets/backgrounds/sea_rainy.png");
       source = require("../assets/icons/rain2.png");
+      bgcolor = "#57575D";
       break;
     case "Snow":
       bgsource = require("../assets/backgrounds/sea_rainy.png");
       source = require("../assets/icons/rain2.png");
+      bgcolor = "#57575D";
       break;
     case "Drizzle":
       bgsource = require("../assets/backgrounds/sea_rainy.png");
       source = require("../assets/icons/rain2.png");
+      bgcolor = "#57575D";
       break;
     case "Haze":
       bgsource = require("../assets/backgrounds/sea_rainy.png");
       source = require("../assets/icons/rain2.png");
+      bgcolor = "#57575D";
       break;
     case "Mist":
       bgsource = require("../assets/backgrounds/sea_rainy.png");
       source = require("../assets/icons/rain2.png");
+      bgcolor = "#57575D";
       break;
     default:
-      bgsource = require("../assets/backgrounds/sea_rainy.png");
+      bgsource = require("../assets/backgrounds/sea_sunnypng.png");
       source = require("../assets/icons/clear2.png");
       break;
   }
@@ -163,39 +188,19 @@ const HomeScreen = ({ navigation }) => {
   let offset = 1;
   const Item = ({ title }) => (
     <View
-      style={{
-        opacity: 0.9,
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
+      style={[
+        styles.itemcontainer,
+        {
+          marginTop: StatusBar.currentHeight || 0,
+        },
+      ]}
     >
-      <View style={{ width: "27%" }}>
-        <Text
-          style={{
-            fontWeight: "300",
-            marginBottom: 20,
-            color: "white",
-            textAlign: "left",
-            fontSize: 20,
-          }}
-        >
-          {dayBuilder(new Date(), offset++)}
-        </Text>
+      <View style={{ width: "30%" }}>
+        <Text style={styles.itemtext}>{dayBuilder(new Date(), offset++)}</Text>
       </View>
       <View>
-        {/* <Icon
-          style={styles.icon}
-          name={
-            weatherDetails.wea === undefined
-              ? "circle-off-outline"
-              : weatherConditions[weatherDetails.wea]?.icon
-          }
-          size={18}
-          color={"white"}
-        /> */}
-
         {weatherDetails.wea !== undefined ? (
-          <Image style={[styles.icon, { marginBottom: 20 }]} source={source} />
+          <Image style={{ marginBottom: 20 }} source={source} />
         ) : (
           <Image
             style={styles.icon}
@@ -204,59 +209,10 @@ const HomeScreen = ({ navigation }) => {
         )}
       </View>
       <View style={{ width: "30%" }}>
-        <Text
-          style={{
-            fontWeight: "300",
-            marginBottom: 20,
-            color: "white",
-            textAlign: "right",
-            fontSize: 20,
-          }}
-        >
-          {title}°
-        </Text>
+        <Text style={styles.itemtemp}>{title}°</Text>
       </View>
     </View>
   );
-
-  const search = () => {
-    // setLoading(true);
-    // fetch(
-    //   `${api.base}weather?lat=${coordinates.lat}&lon=${coordinates.long}&units=metric&APPID=` +
-    //     "adee2681c1b31134e040b6e42bd659d1"
-    // )
-    //   .then((res) => res.json())
-    //   .then((result) => {
-    //     console.log("api results: ", result);
-    //     if (result.name !== null) {
-    //       setWeatherDetails({
-    //         temp: Math.round(result.main.temp),
-    //         wea: result.weather[0].main,
-    //         city: result.name,
-    //         country: result.sys.country,
-    //       });
-    //     } else {
-    //       this.props.navigation.navigate("Home");
-    //     }
-    //   });
-    // fetch(
-    //   `${api.base}forecast?lat=${coordinates.lat}&lon=${coordinates.long}&units=metric&APPID=adee2681c1b31134e040b6e42bd659d1`
-    // )
-    //   .then((res) => res.json())
-    //   .then((result) => {
-    //     setLoading(false);
-    //     let forecast_ = [];
-    //     for (let index = 7, k = 1; index < result.cnt; index += 8, k++) {
-    //       let data = {
-    //         key: k,
-    //         temp: Math.round(result.list[index].main.temp),
-    //         wea: result.list[index].weather[0].main,
-    //       };
-    //       forecast_.push(data);
-    //     }
-    //     setForecast(forecast_);
-    //   });
-  };
 
   const dayBuilder = (d, offset) => {
     let days = [
@@ -272,170 +228,45 @@ const HomeScreen = ({ navigation }) => {
     let day = days[(d.getDay() + offset) % 7];
     return `${day}`;
   };
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    return `${day}, ${month} ${date}`;
-  };
   const renderItem = ({ item }) => <Item title={item.temp} />;
   return (
     <>
       {isLoading ? (
         <View style={styles.container}>
-          {/* <LottieView
-            source={require("./anim/loader_animation.json")}
-            autoPlay
-            loop
-          /> */}
           <Text style={{ textAlign: "center" }}>
             Getting Weather Information
           </Text>
           <ActivityIndicator size="large" />
         </View>
       ) : (
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "#fff",
-            justifyContent: "center",
-            alignContent: "center",
-            marginTop: StatusBar.currentHeight || 0,
-          }}
-        >
+        <View style={styles.container}>
           <ImageBackground
-            style={{
-              flex: 0.5,
-              justifyContent: "flex-start",
-              alignItems: "center",
-            }}
+            style={styles.background}
             source={bgsource}
             resizeMode="cover"
           >
-            <Text
-              style={{
-                fontSize: 75,
-                fontWeight: "300",
-                color: "#fff",
-                marginTop: 150,
-                marginBottom: 10,
-              }}
-            >
-              {weatherDetails.temp}°
-            </Text>
+            <Text style={styles.temperature}>{weatherDetails.temp}°</Text>
 
-            <Text
-              style={{
-                fontSize: 40,
-                fontWeight: "500",
-                color: "#fff",
-              }}
-            >
-              {weatherDetails.wea.toUpperCase()}
-            </Text>
-            <Text
-              style={{
-                fontSize: 15,
-                fontWeight: "200",
-                color: "#fff",
-                marginTop: 5,
-                marginBottom: 10,
-              }}
-            >
-              {city}
-            </Text>
+            <Text style={styles.title}>{weatherDetails.wea.toUpperCase()}</Text>
+            <Text style={styles.city}>{weatherDetails.city}</Text>
           </ImageBackground>
-          <View
-            style={{
-              flex: 0.5,
-              backgroundColor: "dodgerblue",
-              // justifyContent: "center",
-              // alignContent: "center",
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                marginHorizontal: 15,
-                marginTop: 10,
-              }}
-            >
-              <Text
-                style={{
-                  fontWeight: "300",
-                  marginBottom: 10,
-                  color: "white",
-                  textAlign: "center",
-                  fontSize: 15,
-                }}
-              >
+          <View style={[styles.row, { backgroundColor: bgcolor }]}>
+            <View style={styles.daytemp}>
+              <Text style={styles.avtemp}>
                 {temperature.min}° {"\n"}min
               </Text>
-              <Text
-                style={{
-                  fontWeight: "300",
-                  marginBottom: 10,
-                  color: "white",
-                  textAlign: "center",
-                  fontSize: 15,
-                }}
-              >
+              <Text style={styles.avtemp}>
                 {weatherDetails.temp}° {"\n"}Current
               </Text>
-              <Text
-                style={{
-                  fontWeight: "300",
-                  marginBottom: 10,
-                  color: "white",
-                  textAlign: "center",
-                  fontSize: 15,
-                }}
-              >
+              <Text style={styles.avtemp}>
                 {temperature.max}° {"\n"}max
               </Text>
             </View>
-            <View
-              style={{
-                marginTop: 5,
-                borderBottomColor: "white",
-                borderBottomWidth: 1,
-              }}
-            />
+            <View style={styles.hr} />
 
             {/* List of dates */}
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                marginHorizontal: 15,
-                marginTop: 10,
-              }}
-            >
+            <View style={styles.forecastContainer}>
               <FlatList
                 style={styles.flatList}
                 data={forecast}
@@ -446,130 +277,9 @@ const HomeScreen = ({ navigation }) => {
           </View>
         </View>
       )}
-
-      {/* <View style={styles.container}>
-        <ImageBackground
-          style={[
-            styles.background,
-            // { backgroundColor: weatherConditions[weatherDetails.wea].color },
-            { backgroundColor: "#f7b733" },
-          ]}
-        >
-          <FlatList
-            style={styles.flatList}
-            data={forecast}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.key}
-          />
-          <TouchableHighlight
-            style={styles.button}
-            onPress={() => console.log("Pressed")}
-            //   onPress={() => search()}
-            underlayColor="yellow"
-          >
-            <View>
-              <Text style={styles.buttonText}>Get weather details</Text>
-            </View>
-          </TouchableHighlight>
-        </ImageBackground>
-      </View> */}
     </>
   );
 };
 // };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignContent: "center",
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  date: {
-    marginTop: 100,
-    fontSize: 30,
-    fontWeight: "300",
-    marginBottom: 10,
-  },
-  icon: {
-    marginTop: 0,
-  },
-  button: {
-    width: "50%",
-    height: 50,
-    marginBottom: 50,
-    backgroundColor: "gold",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    marginTop: 20,
-    shadowColor: "black",
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: "300",
-    color: "black",
-  },
-  temperature: {
-    fontSize: 62,
-    fontWeight: "100",
-    margin: 5,
-    marginTop: 20,
-  },
-  location: {
-    fontSize: 16,
-    fontWeight: "200",
-    marginBottom: 10,
-  },
-  weatherType: {
-    fontSize: 34,
-    fontWeight: "500",
-  },
-  flatList: {
-    width: "100%",
-    marginTop: 20,
-  },
-  item: {
-    backgroundColor: "white",
-    opacity: 0.9,
-    padding: 18,
-    marginVertical: 7,
-    marginHorizontal: 16,
-    borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "baseline",
-    justifyContent: "space-between",
-  },
-  title: {
-    fontSize: 20,
-  },
-  day: {
-    fontSize: 20,
-    fontFamily: "Arial",
-    marginRight: "0%",
-    fontWeight: "200",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#666",
-    height: 40,
-    marginVertical: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-  },
-  background: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-  searchButton: {
-    width: "100%",
-    height: 50,
-    marginBottom: 200,
-    backgroundColor: "#fcf",
-  },
-});
 
 export default HomeScreen;
