47f934a43cb93fa55e151da0b29194ab98525034
hanielchids <hanielchids@gmail.com>
2023-09-04 17:52:24 +0200

    fix(pages): removed additional screens

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./components/HomeScreen";
-import ForecastScreen from "./components/ForecastScreen";
 
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -15,7 +14,6 @@ const App = () => {
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="ForecastScreen" component={ForecastScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
