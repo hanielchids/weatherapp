5c6625baa4ec3287e0c9e2c953c7ef5c522d33e1
hanielchids <hanielchids@gmail.com>
2023-09-04 14:58:22 +0200

    feat(pages): weather conditions object added

---
+++ utils/weatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+  Rain: {
+    color: "#005BEA",
+    title: "Raining",
+    subtitle: "Get a cup of coffee",
+    icon: "rain2.png",
+  },
+  Clear: {
+    color: "#f7b733",
+    title: "So Sunny",
+    subtitle: "It is hurting my eyes",
+    icon: "clear2.png",
+  },
+  Thunderstorm: {
+    color: "#616161",
+    title: "A Storm is coming",
+    subtitle: "Because Gods are angry",
+    icon: "rain2.png",
+  },
+  Clouds: {
+    color: "#966161",
+    title: "Clouds",
+    subtitle: "Everywhere",
+    icon: "partlysunny2.png",
+  },
+
+  Snow: {
+    color: "#00d2ff",
+    title: "Snow",
+    subtitle: "Get out and build a snowman for me",
+    icon: "rain2.png",
+  },
+  Drizzle: {
+    color: "#076585",
+    title: "Drizzle",
+    subtitle: "Partially raining...",
+    icon: "rain2.png",
+  },
+  Haze: {
+    color: "#66A6FF",
+    title: "Haze",
+    subtitle: "Another name for Partial Raining",
+    icon: "rain2.png",
+  },
+  Mist: {
+    color: "#3CD3AD",
+    title: "Mist",
+    subtitle: "Don't roam in forests!",
+    icon: "rain2.png",
+  },
+};
